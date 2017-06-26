3268181c899c7e5df493cb303d2fa904b532d1aa
Archit Yadav <archityadav97@gmail.com>
2017-06-26 14:18:56 +0530

    Making of 2nd order PLL
    Few minor chanes have been done, like converting the DigitalLoopFilter
    from a normal script to a function. 2nd order terms have been added too.

--- Bilinear_Integrator.m
+++ Bilinear_Integrator.m
@@ -1,8 +1,8 @@
-function [ a ] = Bilinear_Integrator( phaseError, n, T, k )
+function [ a ] = Bilinear_Integrator( phaseError, n, T)
 % Digital Bilinear Transform Integrator
 %   Keeps piling(adding) up the values from the input and keeps on taking
 %   the average of the piled up numbers.
 
-a = T*k*sum(phaseError(1:n-1)) + k*phaseError(n)/2;
+a = T*sum(phaseError(1:n-1)) + phaseError(n)/2;
 end
 
--- DigitalLoopFilter.m
+++ DigitalLoopFilter.m
@@ -1,11 +1,16 @@
+function [finalOP] = DigitalLoopFilter()
 phaseError = [30, 31, 32, 32, 39, 33, 30];    % input array from the PLL module
-k = 1;  % Second order term (has to be found out and replaced eventualy)
-T = 1;  % Time period of advancement (will be related to the sampling frequency)
-fs = 8*10^6;    % sampling frequency (unused for now)
+Bn = 20;    % Noise bandwidth 20Hz (taken arbitrarily)
+wp = 0.53*Bn;   % Loop filter natural radian frequency
+a2 = (2)^(1/2); % second order coeff. term
+%k = 1;      % Second order term (has to be found out and replaced eventualy)
+T = 1;      % Time period of advancement (will be related to the sampling frequency)
+fs = 8*10^6;% sampling frequency (unused for now)
 %outputVar = [];
 outputVar = zeros(1, 7);    % preallocate the array with zeroes.
 for i = 1:1:7
-    outputVar(1, i) = Bilinear_Integrator(phaseError, i, T, k)
+    outputVar(1, i) = Bilinear_Integrator(phaseError, i, T)
 end
 
-
+finalOP = outputVar*(wp)^2 + a2*wp
+end
