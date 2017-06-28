537e683746cb20058728ebe0f2879e328e0fe36f
Archit Yadav <archityadav97@gmail.com>
2017-06-28 21:28:39 +0530

    Improvisation of loop filter
    A very silly error in the last line has been fixed. Parameters of the
    filter have been changed too.

--- DigitalLoopFilter.m
+++ DigitalLoopFilter.m
@@ -1,14 +1,14 @@
-function [finalOP, ap] = DigitalLoopFilter(ap, x, T)
+function [finalOP, a] = DigitalLoopFilter(ap, x, T)
 
-Bn = 20;        % Noise bandwidth 20Hz (taken arbitrarily)
-wp = 0.53*Bn;   % Loop filter natural radian frequency
-a2 = (2)^(1/2); % second order coeff. term
+Bn = 15;        % Noise bandwidth 20Hz (taken arbitrarily)
+wp = Bn/0.53;   % Loop filter natural radian frequency
+a2 = 0.02; % second order coeff. term
 %ap = 0;        % initialized to zero(ths has to be initialzed to 0 in main module, not here)
 %T = 1;         % Time period of advancement (will be related to the sampling frequency)
-fs = 8*10^6;    % sampling frequency (unused for now)
+%fs = 8*10^6;    % sampling frequency (unused for now)
 
 [op, a] = Bilinear_Integrator(x, T, ap);
-ap = a;     % ap is the intermediate term from the bilinear filter.
+%ap = a;     % ap is the intermediate term from the bilinear filter.
 
-finalOP = op*(wp)^2 + a2*wp;
+finalOP = op*(wp)^2 + x*a2*wp;
 end
