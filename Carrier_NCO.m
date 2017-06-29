1da75aaa23ae220266c1843ac9c0c0bd5890b896
Archit Yadav <archityadav97@gmail.com>
2017-06-29 16:20:45 +0530

    Changing time duration
    Limit of time variation has been changed in accordance with GPS signal's
    message time period.

--- Carrier_NCO.m
+++ Carrier_NCO.m
@@ -3,7 +3,8 @@ function [ sinMap, cosMap ] = Carrier_NCO( phaseDev, freqDev )
 %   Detailed explanation goes here
     fs = 8*10^6;    % sampling frequency
     fc = 2*10^6;
-    t = 0:(1/fs):(4000/fs-1/fs); % one way of varying time.
+    t = 0:(1/fs):(8000/fs-1/fs); % one way of varying time.
+    %8K/fs was taken so as to get 1ms length of data (1s->8M; 1ms->8M*1m)
     sinMap = sin(2*pi*(fc + freqDev)*t + phaseDev);
     cosMap = cos(2*pi*(fc + freqDev)*t + phaseDev);
 
--- SineGen.m
+++ SineGen.m
@@ -3,7 +3,8 @@ function [ sinMap ] = SineGen( phaseDev, freqDev )
 %   Detailed explanation goes here
     
     fs = 8*10^6;    % sampling frequency
-    t = 0:(1/fs):(4000/fs-1/fs); % one way of varying time.
+    t = 0:(1/fs):(8000/fs-1/fs); % one way of varying time.
+    %8K/fs was taken so as to get 1ms length of data (1s->8M; 1ms->8M*1m)
     fc = 2*10^6;
     sinMap = sin(2*pi*(fc + freqDev)*t + phaseDev);
     %values of freqDev and phaseDev will be passed from the main module.
