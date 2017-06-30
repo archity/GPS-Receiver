30adf18ef14c31ce869ccb5694d689c4e7e17819
Archit Yadav <archityadav97@gmail.com>
2017-06-30 14:59:50 +0530

    Noise testing
    Testing of noise is done by generating random signals and adding them to
    the sine wave.

--- SineGen.m
+++ SineGen.m
@@ -1,12 +1,13 @@
 function [ sinMap ] = SineGen( phaseDev, freqDev )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-    
-    fs = 8*10^6;    % sampling frequency
-    t = 0:(1/fs):(8000/fs-1/fs); % one way of varying time.
+    SNR = 15;
+    snr = 10^(SNR/10);
+    fs = 20*10^6;    % sampling frequency
+    t = 0:(1/fs):(800/fs-1/fs); % one way of varying time.
     %8K/fs was taken so as to get 1ms length of data (1s->8M; 1ms->8M*1m)
     fc = 2*10^6;
-    sinMap = sin(2*pi*(fc + freqDev)*t + phaseDev);
+    w = (1/sqrt(snr))*randn(size(t));
+    sinMap = sin(2*pi*(fc + freqDev)*t + phaseDev) + w;
     %values of freqDev and phaseDev will be passed from the main module.
 end
-
