4763620c85fa06b6a102ead8916c462e086c9014
Archit Yadav <archityadav97@gmail.com>
2017-07-07 12:34:07 +0530

    Correction in SinGen
    SinGen fucntion has been modified to work correctly for one block of
    sample completely.
    Main module's loop is being made to run 1 time only for testing.
    3rd algorithm has been put to use, which is giving correct results with
    a linear error.

--- FrequencyFind_FLL.m
+++ FrequencyFind_FLL.m
@@ -4,23 +4,23 @@ function[ calcFreq ] = FrequencyFind_FLL(initSine, sinMap, cosMap, sinMap2, cosM
 fs = 8*10^6;    % sampling frequency
 %%
 
-Q1 = initSine.*cosMap;
-I1 = initSine.*sinMap;
-Q2 = initSine.*cosMap2;
-I2 = initSine.*sinMap2;
-Qps1 = sum(Q1);
-Ips1 = sum(I1);
-Qps2 = sum(Q2);
-Ips2 = sum(I2);
+Q1 = initSine(1:8000).*cosMap;
+I1 = initSine(1:8000).*sinMap;
+Q2 = initSine(8001:16000).*cosMap2;
+I2 = initSine(8001:16000).*sinMap2;
+Qps1 = sum(Q1)/length(Q1);
+Ips1 = sum(I1)/length(I1);
+Qps2 = sum(Q2)/length(Q2);
+Ips2 = sum(I2)/length(I2);
 %%
-dot = Ips1*Ips2 + Qps1*Qps2;
+% dot = Ips1*Ips2 + Qps1*Qps2;
 cross = Ips1*Qps2 - Ips2*Qps1;
 
 % calcFreq = atan2(dot, cross)/(8000/fs);
 
-met2 = cross*sign(dot);
-calcFreq =(met2)/(8000/fs);
+% met2 = cross*sign(dot);
+% calcFreq =(met2)/(8000/fs);
 
-% calcFreq = asin(cross)*10^(3);
+ calcFreq = asin(cross)/(8000/fs);
 
 end
--- Main_Module.m
+++ Main_Module.m
@@ -3,13 +3,13 @@
 T = 10^(-3);  % Time period of advancement (1ms)
 ap = 0; % initialized to zero once
 ap2 = 0;
-initSine = SineGen(0, 10);    %Phase=30, freq=100.
+initSine = SineGen(0, 5);    %Phase=30, freq=100.
 [sinMap, cosMap, sinMap2, cosMap2] = Carrier_NCO(0, 0); %initialized to zero
 outputVar1 = zeros(1, 7);    % preallocate the array with zeroes.
 outputVar2 = zeros(1, 7);
 outputVar3 = zeros(1, 7);
 outputVar4 = zeros(1, 7);
-for n = 1:1:200
+for n = 1:1:1
     %calcPhase = PhaseFind_PLL(initSine, sinMap, cosMap);        % PLL
     calcFreq = FrequencyFind_FLL(initSine, sinMap, cosMap, sinMap2, cosMap2);   % FLL
     %outputVar1(1, n) = calcPhase;   %Testing
